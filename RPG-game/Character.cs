120d747c12864d933e3c5039282680e79aa61375
Vojtěch Wilhelm Skyba <skymmel05@gmail.com>
2025-01-03 20:49:49 +0100

    zakladní verze

--- RPG-game/Character.cs
+++ RPG-game/Character.cs
@@ -1,15 +1,8 @@
 namespace RPG_game;
-public class Character
-{
-    public string Name { get; set; }
-    public int Health { get; set; }
-    public int Attack { get; set; }
-    public int Defense { get; set; }
-    public Character(string name, int health = 3, int attack = 0, int defense = 0)
-    {
-        Name = name;
-        Health = health;
-        Attack = attack;
-        Defense = defense;
-    }
+public class Character(string name, int health = 3, int attack = 0, int defense = 0, List<int>? inventory = null) {
+    public string Name { get; set; } = name;
+    public int Health { get; set; } = health;
+    public int Attack { get; set; } = attack;
+    public int Defense { get; set; } = defense;
+    public List<int>? Inventory { get; set; } = inventory;
 }
--- RPG-game/Program.cs
+++ RPG-game/Program.cs
@@ -1,18 +1,15 @@
 ﻿using Spectre.Console;
 namespace RPG_game;
-class Program
-{
+class Program {
     private static Character _player = new("player");
-    static void Main(string[] args)
-    {
+    static void Main(string[] args) {
         Console.Clear();
-        _player = new Character("Skymmel");
+        _player = new Character(AnsiConsole.Prompt(new TextPrompt<string>("What's your name?")));
         PrintStats();
     }
-    private static void PrintStats()
-    {
+    private static void PrintStats() {
         AnsiConsole.Markup($"[bold]{_player.Name.ToUpper()}[/]  ");
-        AnsiConsole.Markup($"[red]{String.Join(" ", new string[_player.Health].Select(_ => "\u2665"))}[/]");
-        AnsiConsole.Markup($"  [yellow]{String.Join(" ", new string[_player.Attack].Select(_ => "\u26a1"))}[/]");
+        AnsiConsole.Markup($"[red]{string.Join(" ", new string[_player.Health].Select(_ => "\u2665"))}[/]  ");
+        AnsiConsole.MarkupLine($"[yellow]{string.Join(" ", new string[_player.Attack].Select(_ => "\u26a1"))}[/]");
     }
 }
